e46c3465ae45b411988ef1652b8d6634775f311d
Paul Rodrigue <paul.john.rodrigue@gmail.com>
2020-02-28 14:33:06 -0600

    everything up til now

---
+++ Assets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 1, -2);
+    public Vector3 rotationOffset = new Vector3();
+    public float cameraSensitivity = 1f;
+
+    Vector3 destination = Vector3.zero;
+
+    void Start() {
+        SetCameraTarget(target);
+    }
+
+    void SetCameraTarget(Transform t) {
+        target = t;
+    }
+
+    void LateUpdate() {
+        FollowTarget();
+    }
+ 
+    void FollowTarget() {
+        //TODO: limit veritcal rotation
+        float horizontalInput = Input.GetAxis("RHorizontal");
+        float verticalInput = Input.GetAxis("RVertical");
+        Quaternion horizontalRotation = Quaternion.AngleAxis(horizontalInput * cameraSensitivity, Vector3.up);
+        Quaternion verticalRotation = Quaternion.AngleAxis(-1f * verticalInput * cameraSensitivity, transform.right);
+        offset =  horizontalRotation * verticalRotation * offset;
+        transform.position = target.position + offset;
+        transform.LookAt(target);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform cameraTransform;
+    public float velocity = 2;
+
+    Rigidbody rigidBody;
+    Animator animator;
+    
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+    }
+
+    void FixedUpdate() {
+        Move();
+    }
+
+    void Move() {
+        float forwardMotion = Input.GetAxis("Vertical");
+        float rightMotion = Input.GetAxis("Horizontal");
+        Vector3 forward = cameraTransform.forward;
+        forward.y = 0;
+        forward.Normalize();
+        Vector3 motion = (forward * forwardMotion) + (cameraTransform.right * rightMotion);
+        if(!motion.Equals(Vector3.zero)) {
+            transform.rotation = Quaternion.LookRotation(motion, Vector3.up);
+        }
+        motion = motion * velocity;
+        animator.SetFloat("Movement Speed", motion.magnitude);
+        Debug.Log(motion.magnitude);
+        //maintain veritcal motion
+        motion.y = rigidBody.velocity.y;
+        rigidBody.velocity = motion;
+    }
+}
